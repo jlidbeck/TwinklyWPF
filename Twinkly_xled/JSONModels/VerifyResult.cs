be4b358ad9d39675bc7ba5a28d8221275668a075
Jonathan Lidbeck <admin@12toneinteractive.com>
2022-01-20 20:17:44 -0800

    Added ResultCode enum to VerifyResult for known error codes

--- Twinkly_xled/JSONModels/VerifyResult.cs
+++ Twinkly_xled/JSONModels/VerifyResult.cs
@@ -5,5 +5,23 @@
     public class VerifyResult
     {
         public int code { get; set; } // 1000 is ok 1104 is not
+
+        public override string ToString() => $"code: {code}";
+        public bool IsOK => code == 1000;
+    }
+
+    public enum ResultCode
+    {
+        Ok                      = 1000,
+        Error                   = 1001,
+        InvalidArgumentValue    = 1101,
+        Error1102               = 1102,
+        Error1103               = 1103,
+        ValueError              = 1104,    // Error - value too long? Or missing required object key?
+        JSONError               = 1105,
+        InvalidArgumentKey      = 1105,
+        Ok1107                  = 1107,     // unknown
+        Ok1108                  = 1108,     // unknown
+        FirmwareUpgradeError    = 1205      // Error with firmware upgrade - SHA1SUM does not match
     }
 }
